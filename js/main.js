3c39744b39c197f191a7264e5a37915a97e5d432
f <f@MacBook-Pro-f.local>
2018-11-12 18:20:33 +0300

    add items

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-(window.onload = function (){
-    console.log(213)
-})()
+// (window.onload = function (){
+    const items = document.querySelectorAll('.item');
+    
+    document.body.onclick = function(e) {
+        let a = e.target.nextElementSibling
+        if(a.classList.contains('item') ) {
+            return console.log('yep')
+        }
+        return console.dir(e.target)
+    }
+    
+    // items.forEach((e) => {
+    //     e.addEventListener('click', (e) => {
+    //        return  console.dir(e.target)
+    //     })
+    // } )
+// })

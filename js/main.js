1647e96aa2148388b6525a0f547f36c261174804
f <f@MacBook-Pro-f.local>
2018-11-13 16:54:05 +0300

    hidden obj/list shake

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-// (window.onload = function (){
+(window.onload = function (){ 
+
     const items = document.querySelectorAll('.item');
+    const listItems = document.querySelectorAll('li')
     
-    document.body.onclick = function(e) {
-        let a = e.target.nextElementSibling
-        if(a.classList.contains('item') ) {
-            return console.log('yep')
-        }
-        return console.dir(e.target)
-    }
-    
-    // items.forEach((e) => {
-    //     e.addEventListener('click', (e) => {
-    //        return  console.dir(e.target)
-    //     })
-    // } )
-// })
+    items.forEach((e) => {
+        e.addEventListener('click', (e) => {
+            const item = e.target
+           if(item.classList.contains('item')) {
+            console.log(item.classList)
+            item.classList.add('found')
+            item.style.opacity = '0';
+        
+                for (let i = 0; i < listItems.length; i++) {
+                    if(item.classList.contains(listItems[i].textContent)){
+                        listItems[i].classList.add('listFound')
+                    }   
+                }
+            }
+        })
+    })
+
+    // function getMatch(a) {
+    //     const  liMatch = listItems.filter(function(e) { e.textContent == a.classList.contains('e')});
+    //     console.log(liMatch)
+    //     return liMatch;
+    // }
+})
